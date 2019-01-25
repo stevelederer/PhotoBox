326e50d9e2a2c779c32a320c396723a62300ac04
Jack Knight <jkknight9@gmail.com>
2019-01-25 08:53:29 -0700

    eidts code views and controllers and createeventcontroller

---
+++ PhotoBox/EnterCodeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EnterCodeViewController.swift
+//  PhotoBox
+//
+//  Created by Jack Knight on 1/24/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import UIKit
+
+class EnterCodeViewController: UIViewController {
+
+    
+    @IBOutlet weak var enterCodeTextField: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func continueButtonTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
