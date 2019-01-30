9a6a8a81e76dbd3c8164df9cbd516f273a88a2d0
Steve Lederer <stevieled@gmail.com>
2019-01-29 17:00:51 -0700

    Head Fake Git

--- PhotoBox/MessageComposer.swift
+++ PhotoBox/MessageComposer.swift
@@ -1,29 +1,29 @@
-//
-//  MessageComposer.swift
-//  PhotoBox
-//
-//  Created by Jack Knight on 1/23/19.
-//  Copyright © 2019 Cameron Milliken. All rights reserved.
-//
+    //
+    //  MessageComposer.swift
+    //  PhotoBox
+    //
+    //  Created by Jack Knight on 1/23/19.
+    //  Copyright © 2019 Cameron Milliken. All rights reserved.
+    //
 
-import Foundation
-import MessageUI
+    import Foundation
+    import MessageUI
 
-class MessageComposer: NSObject, MFMessageComposeViewControllerDelegate{
+    class MessageComposer: NSObject, MFMessageComposeViewControllerDelegate{
         
-    // checks to see if the device can send text messages
-    func canSendText() -> Bool {
-        return MFMessageComposeViewController.canSendText()
+        // checks to see if the device can send text messages
+        func canSendText() -> Bool {
+            return MFMessageComposeViewController.canSendText()
+        }
+        
+        func configuredMessageComposeViewController(eventName: String, code: String, creator: String) -> MFMessageComposeViewController {
+            let messageComposeVC = MFMessageComposeViewController()
+            messageComposeVC.messageComposeDelegate = self
+            messageComposeVC.body = "You’ve been invited to contribute to a PhotoBox! \(creator) has created a Photobox for \(eventName). To add your photos to the collection, open the Photobox app and enter \(code). New to Photobox? No worries! Download the app from the App Store: (APP STORE LINK)"
+            return messageComposeVC
+        }
+        
+        func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+            controller.dismiss(animated: true, completion: nil)
+        }
     }
-    
-    func configuredMessageComposeViewController(eventName: String, code: String, creator: String) -> MFMessageComposeViewController {
-        let messageComposeVC = MFMessageComposeViewController()
-        messageComposeVC.messageComposeDelegate = self
-        messageComposeVC.body = "You’ve been invited to contribute to a PhotoBox! \(creator) has created a Photobox for \(eventName). To add your photos to the collection, open the Photobox app and enter \(code). New to Photobox? No worries! Download the app from the App Store: (APP STORE LINK)"
-        return messageComposeVC
-    }
-    
-    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-        controller.dismiss(animated: true, completion: nil)
-    }
-}
