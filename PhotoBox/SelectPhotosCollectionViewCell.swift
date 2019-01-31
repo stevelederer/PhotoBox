add82d27ecaeea60fe0f8264b7bd08591dcab9ce
Jack Knight <jkknight9@gmail.com>
2019-01-31 16:08:58 -0700

    adds uploads to selectphoto

--- PhotoBox/SelectPhotosCollectionViewCell.swift
+++ PhotoBox/SelectPhotosCollectionViewCell.swift
@@ -8,9 +8,38 @@
 
 import UIKit
 
+protocol SelectPhotosCollectionViewCellDelegate: class {
+    
+    func photoButtonTapped(sender: SelectPhotosCollectionViewCell)
+}
+
 class SelectPhotosCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var checkmark: UIImageView!
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
     
+    weak var delegate: SelectPhotosCollectionViewCellDelegate?
     
+    override var isSelected: Bool {
+        didSet {
+        }
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: Any) {
+        delegate?.photoButtonTapped(sender: self)
+    }
+    
+    func updateViews() {
+        guard let photo = photo else { return }
+        
+        checkmark.isHidden = !photo.isSelected
+        imageButton.setBackgroundImage(photo.image, for: .normal)
+        imageButton.layer.borderWidth = isSelected ? 5 : 0
+        imageButton.layer.borderColor = UIColor.darkGray.cgColor
+    }
 }
